ca1915437c5b768ba3df50fd709905db19764779
bingtaoo <1007530194@qq.com>
2023-01-29 19:38:07 +0800

    4 files modified, 74 added, 77 deleted

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import VueLazyLoad from 'vue3-lazyload';
+// import App from './App';
+import HelloApp from './HelloApp';
+// import '/mock/mock';
+//import router from './router';
+import store from './store';
+
+// import 'vue-awesome/icons/angle-left';
+// import 'vue-awesome/icons/angle-right';
+// import 'vue-awesome/icons/question-circle';
+
+// const app = createApp(App);
+const app = createApp(HelloApp);
+app.use(VueLazyLoad, {
+  loading: './assets/icon/loading-icon.svg',
+  error: './assets/icon/loading-error.svg',
+});
+
+app.use(store);
+//app.use(router);
+//app.template = '<App/>'
+app.mount('#app');
